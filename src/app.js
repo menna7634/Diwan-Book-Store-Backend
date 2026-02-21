45032d3ecc865fb19b64c15e8a0d31740875dde7
Zeyad Shahin <zeyadshahin2015@gmail.com>
2026-02-21 17:37:01 +0200

    implement routers

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(pinoHttp({ logger }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(require('./routes'));
 app.get('/', (req, res) => res.send('API is running'));
 
 
