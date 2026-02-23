989b81ee0e2aa2a710ee59a4d80b3a423d01c5a3
Zeyad Shahin <zeyadshahin2015@gmail.com>
2026-02-23 15:20:46 +0200

    resolve merge confict

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,8 @@ const router = express.Router();
 
 
 router.use('/auth', require('../modules/auth/auth.routes'));
-<<<<<<< author-routes
 router.use('/authors', require('../modules/authors/authors.routes'));
 router.use('/books', require('../modules/books/books.routes'));
-module.exports = router;
-=======
 router.use('/categories', require('../modules/categories/categories.routes'));
 
 module.exports = router;
->>>>>>> main
