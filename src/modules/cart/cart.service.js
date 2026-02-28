2b240364a712ae4addb4355f6f7e7057b44332ac
menna7634 <mennamohammed178@gmail.com>
2026-02-28 08:37:58 +0200

    fix cart service:populate book cover and title

--- src/modules/cart/cart.service.js
+++ src/modules/cart/cart.service.js
@@ -6,17 +6,25 @@ const {
 
 const MAX_QTY = 99;
 
-//get or create cart for user
 const getOrCreateCart = async (userId) => {
-  let cart = await Cart.findOne({ user_id: userId });
+  let cart = await Cart.findOne({ user_id: userId }).populate(
+    'items.book_id',
+    'book_title book_cover_url'
+  );
   if (!cart) cart = await Cart.create({ user_id: userId, items: [] });
   return cart;
 };
 
-//find item index in cart
-const findItemIndex = (cart, bookId) =>
-  cart.items.findIndex((i) => i.book_id.toString() === bookId.toString());
+const populateCart = (cart) =>
+  cart.populate('items.book_id', 'book_title book_cover_url');
 
+//find item index in cart
+const findItemIndex = (cart, bookId) => {
+  return cart.items.findIndex((item) => {
+    const id = item.book_id?._id ?? item.book_id;
+    return id?.toString() === bookId.toString();
+  });
+};
 const getCart = async (userId) => {
   const cart = await getOrCreateCart(userId);
 
@@ -41,7 +49,7 @@ const addItem = async (userId, { bookId, quantity, price }) => {
   }
 
   await cart.save();
-  return cart;
+  return populateCart(cart);
 };
 
 const setQuantity = async (userId, bookId, quantity) => {
@@ -51,7 +59,7 @@ const setQuantity = async (userId, bookId, quantity) => {
 
   cart.items[idx].quantity = quantity;
   await cart.save();
-  return cart;
+  return populateCart(cart);
 };
 
 const increaseQuantity = async (userId, bookId, step = 1) => {
@@ -65,7 +73,7 @@ const increaseQuantity = async (userId, bookId, step = 1) => {
   cart.items[idx].quantity = newQty;
 
   await cart.save();
-  return cart;
+  return populateCart(cart);
 };
 
 const decreaseQuantity = async (userId, bookId, step = 1) => {
@@ -82,7 +90,7 @@ const decreaseQuantity = async (userId, bookId, step = 1) => {
   }
 
   await cart.save();
-  return cart;
+  return populateCart(cart);
 };
 
 const removeItem = async (userId, bookId) => {
@@ -92,14 +100,14 @@ const removeItem = async (userId, bookId) => {
 
   cart.items.splice(idx, 1);
   await cart.save();
-  return cart;
+  return populateCart(cart);
 };
 
 const clearCart = async (userId) => {
   const cart = await getOrCreateCart(userId);
   cart.items = [];
   await cart.save();
-  return cart;
+  return populateCart(cart);
 };
 
 module.exports = {
